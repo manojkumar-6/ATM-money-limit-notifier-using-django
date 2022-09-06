f4c9cd16f0da2ba730165c2359762c638a9baf6b
manojkumar-6 <110871711+manojkumar-6@users.noreply.github.com>
2022-09-06 21:26:51 +0530

    Update settings.py

--- jn/jn/settings.py
+++ jn/jn/settings.py
@@ -73,7 +73,7 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS="True"
-EMAIL_HOST_USER="noreplyplease1230@gmail.com"
+EMAIL_HOST_USER=""
 EMAIL_HOST_PASSWORD=""
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
