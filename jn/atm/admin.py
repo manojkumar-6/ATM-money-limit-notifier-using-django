332b5e24a23b6c75e399ae174f146c7073f49f99
manojkumar-6 <110871711+manojkumar-6@users.noreply.github.com>
2023-03-05 18:31:22 +0530

    Update admin.py

--- jn/atm/admin.py
+++ jn/atm/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+#the aminm ndskjfkjdfhjklx;lgsadkljkljdfsuif
 admin.site.register(bankname)
 admin.site.register(Atm)
 admin.site.register(users)
